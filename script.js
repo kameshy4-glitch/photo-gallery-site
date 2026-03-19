364f176f482f6ac88a3ea61a6e27367d62453f53
kameshy4-glitch <kameshy4@gmail.com>
2026-03-19 12:58:42 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function openImage(img) {
+    document.getElementById("popup").style.display = "flex";
+    document.getElementById("popup-img").src = img.src;
+}
+
+function closeImage() {
+    document.getElementById("popup").style.display = "none";
+}
